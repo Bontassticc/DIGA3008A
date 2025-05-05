af02fe03cfc6e31d6845ed99a829ab286a30d121
Bontassticc <bonimorojele@gmail.com>
2025-05-05 22:02:46 +0200

    trying out javascript in HP

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "HI, MY LOVES...";
+    const textElement = document.getElementById("typed-text");
+    let index = 0;
+
+    function typeLetter() {
+        if (index < text.length) {
+            textElement.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeLetter, 80); // 80ms delay between characters
+        }
+    }
+
+    typeLetter();
+});
