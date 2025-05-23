795dff63fbf0c6af667b296f673467022075c383
Bontassticc <bonimorojele@gmail.com>
2025-05-23 20:24:10 +0200

    Reading Progress Bar

--- JS/main.js
+++ JS/main.js
@@ -197,3 +197,9 @@ function searchText() {
   });
 }
 
+window.onscroll = function () {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+};
